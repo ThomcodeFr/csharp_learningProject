8d977574ddb2ef8dc6a1aecd9c214863d0847f31
Thomas MILTON <perso.milton@gmail.com>
2023-10-29 22:37:03 +0100

    add proprieties and method etc.. OOP

---
+++ HarryPotter.Games.UI.Console/Core/Player.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HarryPotter.Games.UI.Console.Core
+{
+    /// <summary>
+    /// Classe qui représente le joueur / la joueuse 
+    /// </summary>
+    public class Player
+    {
+        #region Fields
+        private string email;
+        private string pseudo;
+        private DateOnly dateDeNaissance;
+
+        private Player[] Amis = new Player[3]; // Autre classe, il ne peut avoir que 3 amis max
+        #endregion
+
+        #region Constructeur
+        public Player(string pseudo, DateOnly dateNaissance) 
+        {
+            this.Pseudo = pseudo;
+            this.dateDeNaissance = dateNaissance;   
+        }
+
+        public Player(String pseudo)
+        {
+            this.Pseudo = pseudo;
+
+        }
+
+        public Player(DateOnly dateNaissance)
+        {
+            this.dateDeNaissance = dateNaissance;
+
+        }
+
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Méthode qui permet de changer la position du joueur
+        /// </summary>
+        public void SeDeplacer() 
+        {
+            System.Console.WriteLine("Je me déplace");
+        }
+        #endregion
+
+        #region Public Methods
+        /// <summary>
+        /// Méthode qui permet d'attaquer
+        /// </summary>
+        public void Attaquer(dynamic ennemy)
+        {
+            System.Console.WriteLine("J'attaque l'ennemie {0}", ennemy);
+        }
+        #endregion
+
+        #region Propriétés
+        public DateOnly DateDeNaissance
+        { 
+            get 
+            {
+                return this.dateDeNaissance; // attribut de ma classe
+            }
+            set 
+            {
+                if(value == DateOnly.MinValue)
+                {
+                    throw new ArgumentException("dateDeNaissance");
+                }
+                this.dateDeNaissance = value;  // mot clé C# qui permet de récupérer la valeur saisie
+            }
+        }
+        public string Email { get => email; set => email = value; }
+
+        public string Pseudo { get; set; } = "Hermione";
+
+        #endregion
+    }
+}
--- HarryPotter.Games.UI.Console/Program.cs
+++ HarryPotter.Games.UI.Console/Program.cs
@@ -1,94 +1,326 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-#region ---- FRAMEWORK ----
-// Fonction préparé mais non exécuté
-void AffichageCredits() // Corps de méthode, non exécuté tant que pas utilisé
+
+using HarryPotter.Games.UI.Console.Core;
+using System.Diagnostics;
+
+//string surname = null;
+
+//surname.ToLower();
+
+
+string titre = "Harry Potter game";
+titre = "Un jeu épique !";
+
+string sousTitre = "Harry Potter";
+
+string temporaire = titre;
+titre = sousTitre;
+sousTitre = temporaire;
+
+
+// Ca nous sert à ecrire dans le ouput
+// 27/02/2022: Debug.WriteLine("==> Je teste" + titre);
+
+// Des informations d'explications plus claires !
+
+string titreEnMajuscule = titre.ToUpper();
+Console.WriteLine(titreEnMajuscule);
+
+Console.WriteLine(sousTitre.Substring(0, sousTitre.Length - 2));
+
+// Console.WriteLine(sousTitre.Replace(" !", ""));
+
+#region Framework du projet
+void AfficherItemMenu(string itemMenu, int indexMenu = 1)
 {
-    Console.WriteLine("Thomas & co");
+    string format = "{0}. {1}";
+
+    string resultatFormattage = string.Format(format, indexMenu, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
+    Console.WriteLine(resultatFormattage);
+}
+
+// Affiche les crédits sur la console
+void AffichageCredits()
+{
+    Console.WriteLine("**********");
+    Console.WriteLine("Thomas");
     Console.WriteLine("2023");
+    Console.WriteLine("**********");
 }
 
 void AfficherChoixForces()
 {
-    Console.WriteLine("De quel côté de la force es-tu ?");
+    Console.WriteLine("De quel côté de la force seras-tu ? ");
     Console.WriteLine("1. Du côté lumineux");
-    Console.WriteLine("2. Du côté obscure");
-    Console.WriteLine("3. Je ne suis pas un Jedi");
+    Console.WriteLine("2. Du côté obscur");
+    Console.WriteLine("3. Neutre, pas de force pour moi");
 }
 
 int AfficherForcesEtRetourneSelection()
 {
     AfficherChoixForces();
+
     string saisieForce = Console.ReadLine();
+    
     return int.Parse(saisieForce);
 }
 
 int[,] PrepareGrilleDuJeu()
 {
+    //int positionJoueur = 1;
+
     int[] positions = new int[10];
 
-    // APPROCHE MATRICIELLE
+    //var position = positions[5];
+    //Console.WriteLine(position);
+
+    //bool[] etats = new bool[5];
+    //Console.WriteLine(etats[2]);
+
+    //string[] noms = new string[4];
+    //Console.WriteLine(etats[2]);
+
+    #region Approche matricielle
     int[,] grilleDeJeu = new int[20, 20];
     const int AUCUN_PERSO = -1;
 
-    for (int i = 0; i < grilleDeJeu.GetLength(0); i++) // dimension 0 = ligne 0
+    for (int i = 0; i < grilleDeJeu.GetLength(0); i++)
     {
         for (int j = 0; j < grilleDeJeu.GetLength(1); j++)
         {
             grilleDeJeu[i, j] = AUCUN_PERSO;
         }
     }
+    #endregion
+
     return grilleDeJeu;
 }
 
-void AfficherItemMenu(string itemMenu, int indexMenu)
+void AfficherMenu()
 {
-    string format = "{0}. {1}";
-    string resultatFormatage = string.Format(format, indexMenu, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
-    Console.WriteLine(resultatFormatage);
+    AfficherItemMenu("nouvelle partie");
+    AfficherItemMenu("charger une partie", 2);
+    AfficherItemMenu("crédits", 3);
+    AfficherItemMenu("quitter", 4);
 }
 
+string RecupereSaisieAgeNonVide()
+{
+    bool ageSaisiPasValide = true;
+    string ageSaisie = "";
+    do
+    {
+        Console.WriteLine("Ton âge s'il te plaît ?");
+        ageSaisie = Console.ReadLine();
+
+        ageSaisiPasValide = string.IsNullOrWhiteSpace(ageSaisie);
+         
+    } while (ageSaisiPasValide);
+
+    return ageSaisie;
+}
+
+int DemandeEtRecupereAgeValid()
+{
+    int ageLocalPlayer = -1;
+    bool estAgeValid = false;
+
+    while (!estAgeValid)
+    {
+        string ageSaisie = RecupereSaisieAgeNonVide();
+
+        if(int.TryParse(ageSaisie, out ageLocalPlayer))
+        {
+            
+        }
+
+
+        //try
+        //{
+        //    agePlayer = int.Parse(ageSaisie); // int.MaxValue;
+        //                                      // agePlayer = agePlayer + 10;
+        //}
+        //catch (FormatException ex)
+        //{
+        //    agePlayer = 0;
+        //    throw new AgeNonValideException();
+        //}
+        //finally
+        //{
+        //    Console.WriteLine("Et oui c'est pas bien saisi");
+
+
+        int ageMinimum = 12;
+
+        int comparaison = ageLocalPlayer.CompareTo(ageMinimum);
+        Console.WriteLine(comparaison);
+
+        estAgeValid = comparaison >= 0;
+    }
+    //catch (FormatException ex) 
+    //{
+    //    Console.WriteLine(ex.Message);
+    //    throw;
+    //}
+
+    return ageLocalPlayer;
+}
+
+void AfficherInfoAuSujetAge(int agePlayer = 0)
+{
+    if (agePlayer > 0)
+    {
+        Console.WriteLine("Yes, vous pouvez continuer la saisie !");
+
+        if (agePlayer < 18)
+        {
+            Console.WriteLine("Attention tu n'es pas majeur-e ...");
+        }
+        else if (agePlayer < 40)
+        {
+            Console.WriteLine("Ca va tu n'es pas trop vieux ... !");
+        }
+        else
+        {
+            Console.WriteLine("A priori, tu as au moins 40 ou plus ;)");
+        }
+    }
+}
+
+int RecupereAgeValide()
+{
+    int agePlayer = DemandeEtRecupereAgeValid();
+
+    AfficherInfoAuSujetAge(agePlayer);
+
+    return agePlayer;
+}
+
+DateOnly RecupererEtAfficherDateNaissance()
+{
+    Console.WriteLine("Quelle est ta date de naissance ?");
+    string dateSaisie = Console.ReadLine();
+
+    DateTime dateEtHeureNaissance = DateTime.Parse(dateSaisie);
+
+    DateOnly dateNaissance = DateOnly.FromDateTime(dateEtHeureNaissance); // DateOnly.Parse(dateSaisie);
+
+    Console.WriteLine("Tu as saisi " + dateNaissance);
+    Console.WriteLine("Tu as saisi " + dateNaissance.ToString());
+
+    return dateNaissance;
+}
+
+void AfficherArmes()
+{
+    float puissanceArme = 10;
+    puissanceArme = 15.6f;
+
+    Console.WriteLine(puissanceArme);
+
+    int valeurParDefaultPuissanceArme = 10;
+    // affectation sans cast : puissanceArme = valeurParDefaultPuissanceArme;
+
+    Console.WriteLine("Choisissez votre arme pour démarrer le jeu :");
+
+    // int j = 0;
+
+    // j = j + 1; // 0 + 1
+    // j = j + 1; // 1 + 1
+
+    // j++; // 2 + 1
+
+    for (int i = 0; i < 4; i++)
+    {
+        Console.WriteLine($"{i + 1}. Arme {i + 1}");
+        Console.WriteLine("{0}. Arme {0}", i + 1);
+    }
+}
+
+void AfficherForceSelectionnee()
+{
+    int typeForce = AfficherForcesEtRetourneSelection();
+
+    const int forceLumineuse = 1;
+    const int forceObscur = 2;
+    const int sansForce = 3;
+
+    switch (typeForce)
+    {
+        case forceLumineuse:
+            {
+                Console.WriteLine("Tu as choisi le côté lumineux");
+            }
+            break;
+
+        case forceObscur:
+            {
+                Console.WriteLine("Tu as choisi le côté obscur");
+            }
+            break;
+
+        case sansForce:
+            {
+                Console.WriteLine("Tu n'a pas de choix de force");
+            }
+            break;
+
+        default:
+            {
+                Console.WriteLine("Tu n'a rien choisi comme force");
+            }
+            break;
+    }
+}
 #endregion
 
-#region // ---- HARRY POTTER ECRAN TITRE ----
-string titre = "Harry Potter Game";
-string sousTitre = "un jeu épique !";
-Console.WriteLine(titre + ", " + sousTitre);
+#region Initialiation Moteur de jeu
+#region Approche Tableau de tableau
+//int[][][] grilleDeJeu = new int[20][][];
+
+//for (int i = 0; i < 20; i++)
+//{
+//    grilleDeJeu[i] = new int[20][];
+//    for (int j = 0; j < 20; j++)
+//    {
+//        grilleDeJeu[i][j] = new int[20];
+
+//    }
+//}
 #endregion
 
-#region ---- INITALISATION MOTEUR DE JEU ----
-int[,] grilleDeJeu = PrepareGrilleDuJeu();
+int[,] grille = PrepareGrilleDuJeu();
+
+Player player = new Player("DefaultPseudo"); 
+
 #endregion
 
-#region ---- AFFICHAGE MENU ----
-// Formatage en une étape
+#region MENU
+#region Ancienne version
+// Formattage en une étape
 //string itemMenu = "nouvelle partie";
 //Console.WriteLine(format, 1, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
 
-// Formatage en 2 étapes
-//string itemMenu = "nouvelle partie";
-//string resultatFormatage = string.Format(format, 1, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
-//Console.WriteLine(resultatFormatage);
+//// Formattage en 2 étapes
+////string itemMenu = "nouvelle partie";
+////string resultatFormattage = string.Format(format, 1, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
+////Console.WriteLine(resultatFormattage);
 
 //itemMenu = "charger une partie";
-//string resultatFormatage = $"{2}. {itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower()}";
-//Console.WriteLine(resultatFormatage);
+//string resultatFormattage = $"{2}. {itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower()}"; 
+//Console.WriteLine(resultatFormattage);
 
 //itemMenu = "crédits";
-//resultatFormatage = string.Format(format, 3, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
-//Console.WriteLine(resultatFormatage);
+//resultatFormattage = string.Format(format, 3, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
+//Console.WriteLine(resultatFormattage);
 
 //itemMenu = "quitter";
-//resultatFormatage = string.Format(format, 4, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
-//Console.WriteLine(resultatFormatage);
+//resultatFormattage = string.Format(format, 4, itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
+//Console.WriteLine(resultatFormattage);
 
 
-AfficherItemMenu("nouvelle partie", 1);
-AfficherItemMenu("charger une partie", 2);
-AfficherItemMenu("crédits", 3);
-AfficherItemMenu("quitter", 4);
-
-// Première étape avant formatage
+// Première étape avant formattage
 //string itemMenu = "nouvelle partie";
 //Console.WriteLine(1 + "." + itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
 
@@ -100,7 +332,11 @@ AfficherItemMenu("quitter", 4);
 //Console.WriteLine(4 + "." + itemMenu.Substring(0, 1).ToUpper() + itemMenu.Substring(1).ToLower());
 #endregion
 
+AfficherMenu();
+#endregion
+
 #region PARTIE SAISIE INFORMATIONS JOUEUR / JOUEUSE
+#region Commentaires
 //int j = 0;
 //while (j < 2)
 //{
@@ -116,168 +352,43 @@ AfficherItemMenu("quitter", 4);
 
 //    j++;
 //} while (j < 2);
+#endregion
 
-bool estAgeValid = false;
-int agePlayer = 0;
-
-while (!estAgeValid)
-{
-    #region Verif saisie non vide
-    bool ageSaisiPasValide = true;
-    string ageSaisie = "";
-    do
-    {
-        Console.WriteLine("Ton âge s'il te plaît ?");
-        ageSaisie = Console.ReadLine();
-
-        ageSaisiPasValide = string.IsNullOrWhiteSpace(ageSaisie);
-
-    } while (ageSaisiPasValide);
-    #endregion
-
-    //if (! string.IsNullOrEmpty(ageSaisie)) //== true)
-    //{
-    //    Console.WriteLine("Age bien saisie");
-    //}
-    //else
-    //{
-    //    Console.WriteLine("Âge obligatoire !");
-    //    Environment.Exit(-1);
-    //}
-
-    agePlayer = int.Parse(ageSaisie); // int.MaxValue;
-                                      // agePlayer = agePlayer + 10;
-
-    int ageMinimum = 12;
-
-    int comparaison = agePlayer.CompareTo(ageMinimum);
-    Console.WriteLine(comparaison);
-
-    estAgeValid = comparaison >= 0;
-}
-
-// 3° façon if
-//if (! (comparaison >= 0))
-//{
-//    Console.WriteLine("Âge interdit !");
-//    Environment.Exit(-1);
-//}
-
-// Une ligne sous le if
-//if (!estAgeValid)
-//    Environment.Exit(-1);
-
-// 2° étape if
-// Avant modif avec le while
-// if (! estAgeValid)
-//{
-//    Console.WriteLine("Âge interdit !");
-//    Environment.Exit(-1);
-//}
-if (estAgeValid)
-{
-    Console.WriteLine("Yes, vous pouvez continuer la saisie !");
-
-    if (agePlayer < 18)
-    {
-        Console.WriteLine("Attention tu n'es pas majeur-e ...");
-    }
-    else if (agePlayer < 40)
-    {
-        Console.WriteLine("Ca va tu n'es pas trop vieux ... !");
-    }
-    else
-    {
-        Console.WriteLine("A priori, tu as au moins 40 ou plus ;)");
-    }
-}
-
-// Premiere étape if
-//bool verification = estAgeValid == false;
-//if (verification)
-//{
-//    Console.WriteLine("Âge interdit !");
-//    Environment.Exit(-1);
-//}
-
-//estAgeValid = estAgeValid || false;
-
-// pour info : Console.WriteLine(int.MinValue);
-// pour info : Console.WriteLine(int.MaxValue);
-
-
+int agePlayer = RecupereAgeValide();
 Console.WriteLine(agePlayer);
 #endregion
 
-#region ---- DATE DE NAISSANCE ---- 
-/*
-Console.WriteLine("Quelle est ta date de naissance ?");
-string dateSaisie = Console.ReadLine();
+#region ---- DATE DE NAISSANCE -----
+DateOnly dateNaissance = RecupererEtAfficherDateNaissance();
+player.DateDeNaissance = dateNaissance;
 
-DateTime dateEtHeureNaissance = DateTime.Parse(dateSaisie); // Vérification validité date de naissance
+//Console.WriteLine($"Le player a la date {player.DateDeNaissance}");
 
-DateOnly dateNaissance = DateOnly.FromDateTime(dateEtHeureNaissance);
+//var player2 = new Player();
 
-Console.WriteLine("Tu as saisie " + dateNaissance);
-*/
+//player2 = player;
+//player2.DateDeNaissance = new DateOnly(2000, 1, 2);
+//Console.WriteLine($"Le player 2 a la date {player2.DateDeNaissance}");
+//Console.WriteLine($"Le player a la date {player.DateDeNaissance}");
 #endregion
 
-#region ---- PREPARATION ARME ---- 
-// decimal puissanceArme = 10;
+#region ---- PREPARATION ARME -----
+//decimal puissanceArme = 10;
+AfficherArmes();
 
-float puissanceArme = 10;
-puissanceArme = 15.5f;
-
-Console.WriteLine(puissanceArme);
-
-int valeurParDefaultPuissanceArme = 10;
-
-Console.WriteLine("Choisissez votre arme par défaut pour démarrer le jeu");
-
-for (int i = 0; i < 4; i++)
-{
-    Console.WriteLine($"{i}. Arme {i + 1}");
-    Console.WriteLine($"{i}. Arme {i + 1}");
-}
-
-// affectation sans cast puissanceArme = valeurParDefaultPuissanceArme;
-// force cast int valeurPuissanceX = (int) puissanceArme;
-
+// force cast : int valeurPuissanceX = (int) puissanceArme;
 #endregion
 
-#region ---- CHOIX COTE FORCE ---- 
-int typeForce = AfficherForcesEtRetourneSelection(); 
+#region ---- CHOIX COTE FORCE -----
+AfficherForceSelectionnee();
+#endregion
 
-const int forceLumineuse = 1;
-const int forceObscure = 2;
-const int sansForce = 3;
-
-
-switch (typeForce)
-{
-    case forceLumineuse:
-        {
-            Console.WriteLine("Tu as choisi le côté lumineux");
-        } break;
-    case forceObscure:
-        {
-            Console.WriteLine("Tu as choisi le côté obscur");
-        }
-        break;
-    case sansForce:
-        {
-            Console.WriteLine("Tu n'as pas de choix de force");
-        }
-        break;
-    default:
-        {
-            Console.WriteLine("Tu n'as rien choisi");
-        } break;
-}
-
+#region ---- AFFICHAGE CREDITS ----
+AffichageCredits();
 #endregion
 
 
-#region ---- AFFICHAGE CREDIT ----
-AffichageCredits(); // Execution ! 
-#endregion<
+#region Lancement du jeu
+player.SeDeplacer();
+player.Attaquer(player);
+#endregion
